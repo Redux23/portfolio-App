a1425e5b531cb622a5b7d3a4def3bd58369e7856
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-05-29 17:32:07 +0100

    Added the State props to the App file.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>This is my project</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      title: "My Portfolio",
+      headerLinks: [
+        {title: "Home", path: "/" },
+        {title: "About", path: "/about" },
+        {title: "Contact", path: "/contact" }
+      ],
+      home: {
+        title: "Welcome to my website.",
+        subtitle: `const myName = "Bright Osuagwu; 
+        I am a Frontend Web Developer with skills 
+        that connect like a hook`,
+        text: "Check below to see some of my works"
+      },
+      about: {
+        imgUrl: "",
+        title: "About Me"
+      },
+      contact: {
+        title: "conatct me"
+      }
+
+    }
+
+
+  }
+  render(){
+
+    return (
+      <div className="App">
+      <h1>This is my project</h1>
+      </div>
+    );
+
+  }
 }
 
 export default App;
