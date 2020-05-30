ad3dcda48149924be0c537a25966da50c587d2be
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-05-30 18:16:51 +0100

    Changed the title

--- src/App.js
+++ src/App.js
@@ -2,34 +2,8 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state ={
-      title: "My Portfolio",
-      headerLinks: [
-        {title: "Home", path: "/" },
-        {title: "About", path: "/about" },
-        {title: "Contact", path: "/contact" }
-      ],
-      home: {
-        title: "Welcome to my website.",
-        subtitle: `const myName = "Bright Osuagwu; 
-        I am a Frontend Web Developer with skills 
-        that connect like a hook`,
-        text: "Check below to see some of my works"
-      },
-      about: {
-        imgUrl: "",
-        title: "About Me"
-      },
-      contact: {
-        title: "conatct me"
-      }
-
-    }
-
-
-  }
+  
+  
   render(){
 
     return (
