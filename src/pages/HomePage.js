fd26a4cb722e65b7888d87fa03bdf7307dbd9153
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-06-08 06:33:23 +0100

    Added the Carousel element component to be rendered.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,12 +7,10 @@ function HomePage(props) {
     return (
         <div>
             <Hero
-
                 title={props.title}
                 subtitle={props.subtitle}
                 text={props.text}
             />
-
             <Carousel />
 
         </div>
