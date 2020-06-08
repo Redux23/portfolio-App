8e4fc25c574c04d4d40cf7f6072808e42249bc15
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-06-08 06:32:25 +0100

    Added card props.

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,18 +1,19 @@
 import React from 'react';
-// import react from '../assets/images/react.png';
+
 
 
 function Card(props) {
+  
     return (
 
         <div className="card text-center">
             <div className="overflow">
-                <img src={props.imgSrc} alt="react-logo" /*className="card-img-top"*//>
+                <img src={props.item.imgSrc} alt="react-logo" /*className="card-img-top"*/ />
             </div>
             <div className="card-body text-dark">
-                <h4 className="card-title">{props.title}</h4>
-    <p className="card-text text-secondary">{props.subTitle}</p>
-                <a href="#" className="btn btn-dark btn-lg">Go Anywhere</a>
+                <h4 className="card-title">{props.item.title}</h4>
+                <p className="card-text text-secondary">{props.item.subTitle}</p>
+                <a href={props.item.link} className="btn btn-dark btn-lg">Click Me</a>
             </div>
         </div>
     )
