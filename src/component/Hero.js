062bc850ecd5fa5975ff2fd3314e16797113f627
Redux23 <bo.osuagwu@mail.comclear$ clear>
2020-06-22 07:58:20 +0100

    Added animated class styling to the banner into.

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -22,9 +22,9 @@ const styles = [
             <Container fluid={true} >
                 <Row className="justify-content-center p-5">
                     <Col md={8} sm={12} className="text-center">
-                        {props.title && <h3 style={styles[0]} className="display-3 font-weight-bolder big-text">{props.title}</h3>}
-                        {props.subtitle && <h4 style={styles[1]} className="display-4">{props.subtitle}</h4>}
-                        {props.text && <h4 className="font-weight-light">{props.text}</h4>}
+                        {props.title && <h3 style={styles[0]} className="display-3 font-weight-bolder big-text animate__animated  animate__slideInLeft">{props.title}</h3>}
+                        {props.subtitle && <h4 style={styles[1]} className="display-4 animate__animated animate__lightSpeedInRight">{props.subtitle}</h4>}
+                        {props.text && <h4 className="font-weight-light animate__animated animate__zoomIn">{props.text}</h4>}
                     </Col>
                 </Row>
             </Container>
